231d41380fb1c752f3a056b922dc046b7f50dcf9
meryvera <yrem.vera.5@gmail.com>
2021-01-27 09:43:57 -0500

    proyecto listo para feedback

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,15 @@
 const cipher = {
   encode: function (offset, encodetext){
+/*     const alphabet = new Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"); */
     let encodetextarrayjs = encodetext.split("");
+/*     let text_resultN = encodetextarrayjs.includes("Ñ");
+    let text_resultn = encodetextarrayjs.includes("ñ");
+
+    if(text_resultN=== true || text_resultn===true){
+      return alert('No debes ingresar ni "Ñ" ni "ñ"');
+    } */
+
     for (let i = 0; i < encodetextarrayjs.length; i++) {
       if(encodetextarrayjs[i] === encodetextarrayjs[i].toUpperCase()){
         encodetextarrayjs[i] = encodetextarrayjs[i].charCodeAt(0);
@@ -23,19 +32,24 @@ const cipher = {
     },
 
   decode: function (number1, text1){
-    let decodetextarrjs = text1.split('');
-    console.log(decodetextarrjs);
+    let decodetextarrayjs = text1.split('');
+/*     let text_resultN = decodetextarrayjs.includes("Ñ");
+    let text_resultn = decodetextarrayjs.includes("ñ");
 
-    for (let index = 0; index <decodetextarrjs.length; index++) {
-      if (decodetextarrjs[index] === decodetextarrjs[index].toUpperCase()) {
-        decodetextarrjs[index] = decodetextarrjs[index].charCodeAt(0);
+    if(text_resultN=== true || text_resultn===true){
+      return alert('No debes ingresar ni "Ñ" ni "ñ"');
+    } */
+
+    for (let index = 0; index < decodetextarrayjs.length; index++) {
+      if (decodetextarrayjs[index] === decodetextarrayjs[index].toUpperCase()) {
+        decodetextarrayjs[index] = decodetextarrayjs[index].charCodeAt(0);
       }
-      else if (decodetextarrjs[index] === decodetextarrjs[index].toLowerCase()) {
-        decodetextarrjs[index] = decodetextarrjs[index].toUpperCase(0);
-        decodetextarrjs[index] = decodetextarrjs[index].charCodeAt(0);
+      else if (decodetextarrayjs[index] === decodetextarrayjs[index].toLowerCase()) {
+        decodetextarrayjs[index] = decodetextarrayjs[index].toUpperCase();
+        decodetextarrayjs[index] = decodetextarrayjs[index].charCodeAt(0);
       }
     }
-    let rundecodetextarrjs = decodetextarrjs.map(function (elemento){
+    let rundecodetextarrjs = decodetextarrayjs.map(function (elemento){
       return String.fromCharCode((elemento - number1 + 65 )% 26 + 65)
     });
     let stringconvert1 = rundecodetextarrjs.join('');
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -27,7 +27,7 @@ describe('cipher', () => {
 
     it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
       expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
-    });
+    }); //la funcion cipher acepta solo 2 parametros , numero y string, x ende no debo pasar 1 array defrente
 
     // Hacker edition
     //
@@ -53,9 +53,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // it('should return " !@" for " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    //  it('should return " !@" for " !@"', () => {
+    //     expect(cipher.encode(33, ' !@')).toBe(' !@');
+    //  });
   });
 
   describe('cipher.decode', () => {
